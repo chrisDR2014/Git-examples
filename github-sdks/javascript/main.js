d68e27940d1c36f1fb88308db06610808b5a0e6b
ChrisDR2024 <162768245+ChrisDR2024@users.noreply.github.com>
2024-03-24 01:08:24 +0000

    JS CLI config

--- github-sdks/javascript/main.js
+++ github-sdks/javascript/main.js
@@ -1,16 +1,16 @@
 // Octokit.js
-import { Octokit, App } from "octokit";
+const { Octokit, App } = require("octokit");
 
 // https://github.com/octokit/core.js#readme
 const octokit = new Octokit({
-    auth: 'process.env.GITHUB_TOKEN'
+    auth: process.env.GITHUB_TOKEN
   })
   
-  await octokit.request('POST /repos/{owner}/{repo}/git/refs', {
+  octokit.request('POST /repos/{owner}/{repo}/git/refs', {
     owner: 'ChrisDR2024',
     repo: 'Git-Examples',
     ref: 'refs/heads/sdksjs',
-    sha: 'e9ea29406884888dbe5822874f4c0f8c947ffcbc',
+    sha: '31f0366486261e85937c7438c9110984d587105b',
     headers: {
       'X-GitHub-Api-Version': '2022-11-28'
     }
